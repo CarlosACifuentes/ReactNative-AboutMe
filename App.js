e54a9abe06f5357d15956edebe7dba18efab69b6
Carlos Cifuentes <cabetocifuentes@gmail.com>
2021-09-15 11:12:48 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
+
+
+export default function HelloWorldApp(){
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+     <Text>Carlos Cifuentes</Text>
+     <Text>Entrepreneur—>Soon</Text>
+     </View>
+    );
+}
